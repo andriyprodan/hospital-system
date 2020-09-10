8bae9555ac55fad0f7c3fcf4b1725c91cdd35b61
andriyprodan <prodandgri@gmail.com>
2020-09-10 16:01:27 +0300

    Add StaffRequiredMixin to DoctorSignUpView

---
+++ project/hospital_auth/mixins.py
@@ -0,0 +1,7 @@
+from django.contrib.admin.views.decorators import staff_member_required
+
+class StaffRequiredMixin(object):
+    @classmethod
+    def as_view(self, *args, **kwargs):
+        view = super().as_view(*args, **kwargs)
+        return staff_member_required(view)
