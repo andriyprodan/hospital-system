dc15ee13d08b9a458cec6881288c51b09c68cd67
andriyprodan <prodandgri@gmail.com>
2020-09-11 10:11:12 +0300

    Remove username fields from User model.
    Now users can log in using only email. Overwrite UserManager and
    User model. Remove EmailBackend from hospital_auth/backends.py

--- project/hospital_auth/admin.py
+++ project/hospital_auth/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 from .models import Specialization, User, Doctor, Patient
 
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_doctor', 'is_patient',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_doctor', 'is_patient')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
 admin.site.register(Specialization)
-admin.site.register(User)
 admin.site.register(Doctor)
 admin.site.register(Patient)
--- project/hospital_auth/models.py
+++ project/hospital_auth/models.py
@@ -1,12 +1,56 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext as _
 
+
+class UserManager(BaseUserManager):
+    """
+    UserManager for User model with no username field
+    """
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
 class User(AbstractUser):
+
+    # User model uses email for authorization
+    username = None
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     phone = models.CharField(max_length=32)
 
     is_doctor = models.BooleanField('doctor status', default=False)
     is_patient = models.BooleanField('patient status', default=False)
+
 
 class Specialization(models.Model):
     name = models.CharField(max_length=128, verbose_name=_("Specialization name"))
