503f622b39cc1860eb9308460bbe04ca89a54b34
andriyprodan <prodandgri@gmail.com>
2020-09-22 13:52:45 +0300

    Add reviewing of doctor's patients' list

--- project/hospital_auth/urls.py
+++ project/hospital_auth/urls.py
@@ -6,6 +6,7 @@ from .views import (
     DoctorSignUpView,
     LoginView,
     logout,
+    DoctorPatientListView,
 )
 
 app_name = 'hospital_auth'
@@ -14,4 +15,6 @@ urlpatterns = [
     path('register/doctor/', DoctorSignUpView.as_view(), name='register-doctor'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='hospital_auth/logout.html'), name='logout'),
+    # patiens of a particular doctor
+    path('doctor/<int:pk>/patients/', DoctorPatientListView.as_view(), name='doctor_patients'),
 ]
--- project/hospital_auth/views.py
+++ project/hospital_auth/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import login, logout, views as auth_views
-from django.shortcuts import redirect
-from django.views.generic import CreateView
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 
 from .forms import DoctorSignUpForm, PatientSignUpForm, LoginForm
-from .models import User
-from .mixins import StaffRequiredMixin
+from .models import User, Doctor, Patient
+from .mixins import StaffRequiredMixin, DoctorRequiredMixin
+from .decorators import doctor_required
 
 class DoctorSignUpView(StaffRequiredMixin, CreateView):
     """
@@ -44,3 +45,17 @@ def logout_view(request):
     logout(request)
 
     return redirect('hospital_records:home')
+
+class DoctorPatientListView(DoctorRequiredMixin, ListView):
+    login_required = True
+    model = Patient
+    template_name = 'hospital_auth/doctor_patients.html'
+
+    def get_queryset(self):
+        # get patients that served only by the authenticated doctor
+        object_list = Patient.objects.filter(doctors__pk=
+            self.request.user.doctor.id)
+
+        print(object_list)
+
+        return object_list 
